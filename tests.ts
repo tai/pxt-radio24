b83da7bc8a5f45fe93de398cf9affde4ea47bee5
Taisuke Yamada <tai@rakugaki.org>
2020-05-28 01:30:26 +0900

    update

--- tests.ts
+++ tests.ts
@@ -3,11 +3,10 @@ basic.clearScreen()
 radio24.enable()
 
 input.onButtonPressed(Button.A, () => {
-    basic.showString("AAA")
-    radio24.toggle(DigitalPin.P0)
+    basic.showString("A")
 })
 
 input.onButtonPressed(Button.B, () => {
-    let val = radio24.add10(HelloNumber.FOO)
-    basic.showNumber(val)
+    basic.showString("B")
+    radio24.ping()
 })
